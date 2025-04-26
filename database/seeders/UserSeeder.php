33e2aedf9bd603b45472a113c5a19e5962921ca0
Kevin <keving.kpg@gmail.com>
2025-04-26 00:32:56 -0500

    Remove created_at and updated_at fields from UserSeeder for cleaner data insertion

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -22,8 +22,6 @@ class UserSeeder extends Seeder
                 'email_verified_at' => Carbon::now(),
                 'password' => Hash::make('12345678'),
                 'remember_token' => null,
-                'created_at' => now(),
-                'updated_at' => now(),
             ],
             
             [
@@ -32,8 +30,6 @@ class UserSeeder extends Seeder
                 'email_verified_at' => null,
                 'password' => Hash::make('12345678'),
                 'remember_token' => null,
-                'created_at' => now(),
-                'updated_at' => now(),
             ],
 
             [
@@ -42,8 +38,6 @@ class UserSeeder extends Seeder
                 'email_verified_at' => Carbon::now(),
                 'password' => Hash::make('1234'),
                 'remember_token' => null,
-                'created_at' => now(),
-                'updated_at' => now(),
             ],
 
         ];
